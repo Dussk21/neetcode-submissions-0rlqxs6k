abf8ca1cc6c56b27f4bb58d631c5ab8c40d77d7a
Dussk21 <65752466+Dussk21@users.noreply.github.com>
2026-06-19 18:59:32 +0300

    Add: find-in-mountain-array - submission-29

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-29.py
@@ -0,0 +1,47 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        def find_peek(arr):
+            l, r = 0, arr.length() - 1
+            while l < r:
+                m = l + (r-l) // 2
+                if arr.get(m) > arr.get(m+1):
+                    r = m
+                else:
+                    l = m + 1
+            return l
+
+        peek = find_peek(mountainArr)
+        
+        def search_left(pk, arr):
+            l, r = 0, pk
+            while l <= r:
+                m = l + (r-l) // 2
+                val = arr.get(m)
+                if val == target:
+                    return m
+                elif val > target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return -1
+
+        def search_right(pk, arr):
+            l, r = pk+1, arr.length()-1
+            while l <= r:
+                m = l + (r-l) // 2
+                val = arr.get(m)
+                if val == target:
+                    return m
+                elif val < target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            return -1
+        
+        left = search_left(peek, mountainArr)
+        # right = search_right(peek, mountainArr)
+        if left != -1:
+            return left
+        else:
+            return search_right(peek, mountainArr)
+                
